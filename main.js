05b7ac8a983d908d00242959064595175ca2de82
Muhammad Talha <talhaguy@gmail.com>
2021-05-01 00:39:52 -0700

    adding angular app

--- main.js
+++ main.js
@@ -3,14 +3,23 @@ const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 1000,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  win.loadFile('index.html')
+  win.webContents.openDevTools()
+  win.loadFile('ng-fe-pomodoro/dist/ng-fe-pomodoro/index.html')
+
+  // https://github.com/electron/electron/issues/14978
+  // Reloading with Angular router causes page to not load.
+  // This will just load the index html on load fail.
+  win.webContents.on('did-fail-load', () => {
+    console.log('did-fail-load - reload app');
+    win.loadURL(`file://${__dirname}/ng-fe-pomodoro/dist/ng-fe-pomodoro/index.html`);
+  });
 }
 
 app.whenReady().then(() => {
--- preload.js
+++ preload.js
@@ -1,10 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
   for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+    console.log(`${type}-version`, process.versions[type])
   }
 })
+
+/*
+// use contextBridge to expose apis to renderer
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld('api', {
+  baseHref: __dirname
+})
+*/
