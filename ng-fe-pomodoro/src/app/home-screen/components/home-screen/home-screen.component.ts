043baff215893d02140cfda0ba1eb848f8445bc4
Muhammad Talha <talhaguy@gmail.com>
2021-05-02 13:05:24 -0700

    change background color based on interval type

--- ng-fe-pomodoro/src/app/home-screen/components/home-screen/home-screen.component.ts
+++ ng-fe-pomodoro/src/app/home-screen/components/home-screen/home-screen.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { IntervalType } from 'src/app/shared/constants/interval-type.enum';
+import { TimerStateService } from 'src/app/shared/services/timer-state.service';
 
 @Component({
-  selector: 'app-home-screen',
-  templateUrl: './home-screen.component.html',
-  styleUrls: ['./home-screen.component.scss']
+    selector: 'app-home-screen',
+    templateUrl: './home-screen.component.html',
+    styleUrls: ['./home-screen.component.scss'],
 })
-export class HomeScreenComponent implements OnInit {
+export class HomeScreenComponent implements OnInit, OnDestroy {
+    private unsubscribe = new Subject<void>();
 
-  constructor() { }
+    constructor(
+        private timerStateService: TimerStateService,
+        private renderer: Renderer2,
+        @Inject(DOCUMENT) private document: Document
+    ) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.renderer.addClass(this.document.body, 'focus-body');
 
+        this.timerStateService.intervalType$
+            .pipe(takeUntil(this.unsubscribe))
+            .subscribe((intervalType) => {
+                if (intervalType === IntervalType.Focus) {
+                    this.renderer.addClass(this.document.body, 'focus-body');
+                    this.renderer.removeClass(this.document.body, 'break-body');
+                } else {
+                    this.renderer.addClass(this.document.body, 'break-body');
+                    this.renderer.removeClass(this.document.body, 'focus-body');
+                }
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.unsubscribe.next();
+        this.unsubscribe.complete();
+    }
 }
