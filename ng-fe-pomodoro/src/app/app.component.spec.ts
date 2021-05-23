c1b690dc0a473d2b4bfff0f268c9b8b1b38025f5
Muhammad Talha <talhaguy@gmail.com>
2021-05-22 21:44:19 -0700

    storage service tests

--- ng-fe-pomodoro/src/app/app.component.spec.ts
+++ ng-fe-pomodoro/src/app/app.component.spec.ts
@@ -15,19 +15,4 @@ describe('AppComponent', () => {
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
-
-    it(`should have as title 'ng-fe-pomodoro'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('ng-fe-pomodoro');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain(
-            'ng-fe-pomodoro app is running!'
-        );
-    });
 });
--- ng-fe-pomodoro/src/app/shared/services/storage.service.spec.ts
+++ ng-fe-pomodoro/src/app/shared/services/storage.service.spec.ts
@@ -1,16 +1,59 @@
 import { TestBed } from '@angular/core/testing';
+import { API, SaveData } from '@app/shared';
+import { ApiToken } from '../injection-tokens/api.injection-token';
 
 import { StorageService } from './storage.service';
 
 describe('StorageServiceService', () => {
     let service: StorageService;
 
+    const api: API = {
+        storage: {
+            getData: jest.fn().mockResolvedValue({ intervalsCompleted: 5 }),
+            saveData: jest.fn().mockResolvedValue(undefined),
+        },
+    };
+
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [
+                {
+                    provide: ApiToken,
+                    useValue: api,
+                },
+            ],
+        });
         service = TestBed.inject(StorageService);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    describe('saveData()', () => {
+        it('should save given data to storage', (done) => {
+            const data: SaveData = {
+                intervalsCompleted: 1,
+            };
+
+            service.saveData(data).subscribe(() => {
+                expect(api.storage.saveData).toHaveBeenCalledWith(data);
+                done();
+            });
+        });
+    });
+
+    describe('getData()', () => {
+        it('should retrieve data from storage', (done) => {
+            service.getData().subscribe((data) => {
+                expect(api.storage.getData).toHaveBeenCalled();
+                expect(data.intervalsCompleted).toBe(5);
+                done();
+            });
+        });
     });
 });
