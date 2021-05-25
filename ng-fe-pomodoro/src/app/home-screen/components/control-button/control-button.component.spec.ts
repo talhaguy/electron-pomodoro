0cf49fa29acd03b60006e7c552bce978bd291828
Muhammad Talha <talhaguy@gmail.com>
2021-05-24 21:05:41 -0700

    ControlButtonComponent tests

--- ng-fe-pomodoro/src/app/home-screen/components/control-button/control-button.component.spec.ts
+++ ng-fe-pomodoro/src/app/home-screen/components/control-button/control-button.component.spec.ts
@@ -1,25 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ControlButtonComponent } from './control-button.component';
 
 describe('ControlButtonComponent', () => {
-  let component: ControlButtonComponent;
-  let fixture: ComponentFixture<ControlButtonComponent>;
+    let component: ControlButtonComponent;
+    let fixture: ComponentFixture<ControlButtonComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ControlButtonComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ControlButtonComponent],
+        }).compileComponents();
+    });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ControlButtonComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ControlButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should emit an event on click', () => {
+        const buttonClickSpy = jest.spyOn(component.buttonClick, 'emit');
+        const button = fixture.debugElement.query(By.css('.button'));
+        button.triggerEventHandler('click', null);
+        expect(buttonClickSpy).toHaveBeenCalled();
+    });
 });
