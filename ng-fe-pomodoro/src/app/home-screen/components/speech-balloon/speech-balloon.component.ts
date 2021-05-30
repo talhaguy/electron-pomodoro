97b6cea91e6de4abd287dc716cf6406911ece548
Muhammad Talha <talhaguy@gmail.com>
2021-05-29 20:37:38 -0700

    cleaning up speech balloon component

--- ng-fe-pomodoro/src/app/home-screen/components/speech-balloon/speech-balloon.component.ts
+++ ng-fe-pomodoro/src/app/home-screen/components/speech-balloon/speech-balloon.component.ts
@@ -1,14 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-speech-balloon',
     templateUrl: './speech-balloon.component.html',
     styleUrls: ['./speech-balloon.component.scss'],
 })
-export class SpeechBalloonComponent implements OnInit {
+export class SpeechBalloonComponent {
     @Input() message = '';
-
-    constructor() {}
-
-    ngOnInit(): void {}
 }
