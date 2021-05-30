ccd3addc14e874d011ff3718e6b5c82f04d14421
Muhammad Talha <talhaguy@gmail.com>
2021-05-29 20:40:00 -0700

    fix test in time pipe

--- ng-fe-pomodoro/src/app/home-screen/pipes/time.pipe.spec.ts
+++ ng-fe-pomodoro/src/app/home-screen/pipes/time.pipe.spec.ts
@@ -12,16 +12,16 @@ describe('TimePipe', () => {
         let result = pipe.transform(5000);
         expect(result).toBe('00:05');
 
-        result = pipe.transform(30);
+        result = pipe.transform(30000);
         expect(result).toBe('00:30');
 
-        result = pipe.transform(60);
+        result = pipe.transform(60000);
         expect(result).toBe('01:00');
 
-        result = pipe.transform(1234);
+        result = pipe.transform(1234000);
         expect(result).toBe('20:34');
 
-        result = pipe.transform(456);
+        result = pipe.transform(456000);
         expect(result).toBe('07:36');
     });
 });
