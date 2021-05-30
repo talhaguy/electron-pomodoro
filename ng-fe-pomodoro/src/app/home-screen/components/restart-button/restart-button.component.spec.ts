a4f6724651044f42fcc2bfc818e2a991b8801afd
Muhammad Talha <talhaguy@gmail.com>
2021-05-29 20:19:14 -0700

    restart button tests

--- ng-fe-pomodoro/src/app/home-screen/components/restart-button/restart-button.component.spec.ts
+++ ng-fe-pomodoro/src/app/home-screen/components/restart-button/restart-button.component.spec.ts
@@ -1,25 +1,44 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TimerStateService } from 'src/app/shared/services/timer-state.service';
+import { TimerStateServiceStub } from 'src/app/shared/services/timer-state.service.stub';
 
 import { RestartButtonComponent } from './restart-button.component';
 
 describe('RestartButtonComponent', () => {
-  let component: RestartButtonComponent;
-  let fixture: ComponentFixture<RestartButtonComponent>;
+    let component: RestartButtonComponent;
+    let fixture: ComponentFixture<RestartButtonComponent>;
+    let timerStateService: TimerStateService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RestartButtonComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            providers: [
+                {
+                    provide: TimerStateService,
+                    useClass: TimerStateServiceStub,
+                },
+            ],
+            declarations: [RestartButtonComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RestartButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(RestartButtonComponent);
+        component = fixture.componentInstance;
+        timerStateService = TestBed.inject(TimerStateService);
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    describe('onClick()', () => {
+        it('should reset timer', () => {
+            const resetTimerSpy = jest.spyOn(timerStateService, 'resetTimer');
+            component.onClick();
+            expect(resetTimerSpy).toHaveBeenCalledTimes(1);
+        });
+    });
 });
--- ng-fe-pomodoro/src/app/home-screen/components/restart-button/restart-button.component.ts
+++ ng-fe-pomodoro/src/app/home-screen/components/restart-button/restart-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TimerStateService } from 'src/app/shared/services/timer-state.service';
 
 @Component({
@@ -6,10 +6,8 @@ import { TimerStateService } from 'src/app/shared/services/timer-state.service';
     templateUrl: './restart-button.component.html',
     styleUrls: ['./restart-button.component.scss'],
 })
-export class RestartButtonComponent implements OnInit {
+export class RestartButtonComponent {
     constructor(private timerStateService: TimerStateService) {}
-
-    ngOnInit(): void {}
 
     onClick(): void {
         this.timerStateService.resetTimer();
